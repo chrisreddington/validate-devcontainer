d2b5c67b79cd5a6c82fbc528d41e14576c5c79df
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2024-11-18 11:25:12 +0000

    Increase coverage on utils.ts

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -23,12 +23,75 @@ describe('utils', () => {
       }
       expect(isDevcontainerContent(validContent)).toBeTruthy()
     })
+
+    test('should return false for invalid customizations structure', () => {
+      const invalidContent = {
+        customizations: {
+          vscode: {
+            extensions: 'not-an-array'
+          }
+        }
+      }
+      expect(isDevcontainerContent(invalidContent)).toBeFalsy()
+    })
+
+    test('should return false for invalid tasks structure', () => {
+      const invalidContent = {
+        tasks: {
+          build: 123 // Should be string
+        }
+      }
+      expect(isDevcontainerContent(invalidContent)).toBeFalsy()
+    })
+
+    test('should return false for invalid features structure', () => {
+      const invalidContent = {
+        features: {
+          feature1: 'not-an-object'
+        }
+      }
+      expect(isDevcontainerContent(invalidContent)).toBeFalsy()
+    })
+
+    test('should return false when customizations is not an object', () => {
+      const invalidContent = {
+        customizations: 'not-an-object'
+      }
+      expect(isDevcontainerContent(invalidContent)).toBeFalsy()
+    })
+
+    test('should return false when tasks is not an object', () => {
+      const invalidContent = {
+        tasks: 'not-an-object'
+      }
+      expect(isDevcontainerContent(invalidContent)).toBeFalsy()
+    })
+
+    test('should return false when features is not an object', () => {
+      const invalidContent = {
+        features: 'not-an-object'
+      }
+      expect(isDevcontainerContent(invalidContent)).toBeFalsy()
+    })
   })
 
   describe('stripJsonComments', () => {
     test('should remove single line comments', () => {
       const input = '{\n// comment\n"key": "value"\n}'
       expect(stripJsonComments(input)).toBe('{\n\n"key": "value"\n}')
+    })
+
+    test('should handle multiple comments', () => {
+      const input =
+        '{\n// comment 1\n"key": "value",\n// comment 2\n"key2": "value2"\n}'
+      expect(stripJsonComments(input)).toBe(
+        '{\n\n"key": "value",\n\n"key2": "value2"\n}'
+      )
+    })
+
+    test('should handle comments at the end of lines', () => {
+      const input = '{"key": "value" // comment\n}'
+      expect(stripJsonComments(input)).toBe('{"key": "value" \n}')
     })
   })
 })
